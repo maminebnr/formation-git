4b3b8bb8a753f70b895871c646e7299a583234d0
sanchoknani <knanihoussem1994@gmail.com>
2026-01-14 12:19:33 +0100

    crteation_file

--- tp1/KhalilLaabidi/index.py
+++
@@ -1,4 +0,0 @@
-# simple Python script
-
-name = "Khalil Laabidi"
-print(f"{name} is here")
--- tp1/projetGhassen/index.py
+++
@@ -1,6 +0,0 @@
-# projetGhassen index.py
-
-"""Project starter for projetGhassen."""
-
-if __name__ == "__main__":
-    print("Hello from projetGhassen")
